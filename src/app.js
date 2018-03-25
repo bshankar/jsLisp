bc15db52ccb01a85d8be8bdaf14fce1786da4ee5
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-03-25 16:16:07 +0530

    implement define special form

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const { sexpParser } = require('./lisp')
+const { env } = require('./env')
 
 fs.readFile(process.argv[2], 'utf8', (err, s) => {
   if (err) throw err
-  console.log(sexpParser(s))
+  console.log(sexpParser(s, env))
 })
--- src/env.js
+++ src/env.js
@@ -1,16 +1,23 @@
 const env = {
+  // arithmetic operators
   '+': args => args.reduce((a, e) => a + e),
   '-': args => args.reduce((a, e) => a - e),
   '*': args => args.reduce((a, e) => a * e),
   '/': args => args.reduce((a, e) => a / e),
+  '%': args => args.reduce((a, e) => a % e),
+
+  // comparison operators
   '<': args => args.every((e, i) => i ? args[i - 1] < e : true),
   '>': args => args.every((e, i) => i ? args[i - 1] > e : true),
   '<=': args => args.every((e, i) => i ? args[i - 1] <= e : true),
   '>=': args => args.every((e, i) => i ? args[i - 1] >= e : true),
+
+  // special forms
   'begin': args => args[args.length - 1],
-  'define': args => { this[args[0]] = args[1] }
+  'define': function (args) { this[args[0]] = args[1] }
 }
 
+env.define = env.define.bind(env)
 const find = v => v instanceof Array ? env[v[0]] : env[v]
 
 module.exports = { env, find }
